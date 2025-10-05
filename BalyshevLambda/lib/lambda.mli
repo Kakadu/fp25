6ee51c5f80cf700827999dea6e0740f62d3fa1a5
psiblvdegod <artyombaly@list.ru>
2025-10-05 21:24:30 +0300

    update comments in lambda.mli

--- BalyshevLambda/lib/lambda.mli
+++ BalyshevLambda/lib/lambda.mli
@@ -6,11 +6,9 @@
 
 [@@@ocaml.text "/*"]
 
-(*
-   val var : 'a -> 'a Ast.t
-   val abs : 'a -> 'a Ast.t -> 'a Ast.t
-   val app : 'a Ast.t -> 'a Ast.t -> 'a Ast.t *)
-
+val var : 'a -> 'a Ast.t
+val abs : 'a -> 'a Ast.t -> 'a Ast.t
+val app : 'a Ast.t -> 'a Ast.t -> 'a Ast.t
 val subst : string -> by:string Ast.t -> string Ast.t -> string Ast.t
 
 (** Example lambda expressions *)
@@ -30,23 +28,24 @@ val one : string Ast.t
 val two : string Ast.t
 val three : string Ast.t
 
+(* defines reductions limit *)
 type limit =
   | Limited of int
   | Unlimited
   | Exhausted
 
+(* defines beta reduction strategy *)
 type strat =
   { on_var : strat -> Ast.name -> limit -> Ast.name Ast.t * limit
   ; on_abs : strat -> Ast.name -> Ast.name Ast.t -> limit -> Ast.name Ast.t * limit
   ; on_app : strat -> Ast.name Ast.t -> Ast.name Ast.t -> limit -> Ast.name Ast.t * limit
   }
 
-(*  *)
-
-val ao_small_step_strat : strat
 val apply_strat : strat -> Ast.name Ast.t -> limit -> Ast.name Ast.t * limit
+
+(* predefined strategies *)
+val without_strat : strat
 val ao_strat : strat
 val cbn_strat : strat
 val cbv_strat : strat
 val no_strat : strat
-val without_strat : strat
