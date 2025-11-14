1dcc03bcc1f0c0f1e0feacf521159d45c217f058
Mikhail <svmk17@mail.ru>
2025-11-14 23:03:51 +0300

    tests: add mod with primitive tests

---
+++ Svirukov/lib/tests.ml
@@ -0,0 +1,187 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2021-2024, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
+open Lambda_lib.Print
+open Lambda_lib.Parser
+
+let%expect_test "parse number" =
+  print_string (printer (parse "42" |> Result.get_ok));
+  [%expect {| 42 |}]
+;;
+
+let%expect_test "parse variable" =
+  print_string (printer (parse "x" |> Result.get_ok));
+  [%expect {| Var(x) |}]
+;;
+
+let%expect_test "parse addition" =
+  print_string (printer (parse "2 + 3" |> Result.get_ok));
+  [%expect {| (2 + 3) |}]
+;;
+
+let%expect_test "parse multiplication" =
+  print_string (printer (parse "2 * 3" |> Result.get_ok));
+  [%expect {| (2 * 3) |}]
+;;
+
+let%expect_test "parse parentheses" =
+  print_string (printer (parse "(2 + 3)" |> Result.get_ok));
+  [%expect {| (2 + 3) |}]
+;;
+
+let%expect_test "parse comparison" =
+  print_string (printer (parse "2 > 3" |> Result.get_ok));
+  [%expect {| (2 > 3) |}]
+;;
+
+let%expect_test "parse conditional" =
+  print_string (printer (parse "if 1 > 0 then 2 else 3" |> Result.get_ok));
+  [%expect {| if ((1 > 0)) then (2) else (3) |}]
+;;
+
+let%expect_test "parse let binding" =
+  print_string (printer (parse "let x = 5 in x" |> Result.get_ok));
+  [%expect {| (let x = 5 in Var(x)) |}]
+;;
+
+let%expect_test "parse application" =
+  print_string (printer (parse "f x" |> Result.get_ok));
+  [%expect {| App (Var(f), Var(x)) |}]
+;;
+
+let%expect_test "parse anonymous function" =
+  print_string (printer (parse "fun x -> x + 1" |> Result.get_ok));
+  [%expect {| Fun (Var(x), (Var(x) + 1)) |}]
+;;
+
+let%expect_test "parse negative number" =
+  print_string (printer (parse "-5" |> Result.get_ok));
+  [%expect {| -5 |}]
+;;
+
+let%expect_test "parse complex expression" =
+  print_string (printer (parse "2 + 3 * 4" |> Result.get_ok));
+  [%expect {| (2 + (3 * 4)) |}]
+;;
+
+let%expect_test "parse function with multiple args" =
+  print_string (printer (parse "let add x y = x + y in add 1 2" |> Result.get_ok));
+  [%expect
+    {| (let add = Fun (Var(x), Fun (Var(y), (Var(x) + Var(y)))) in App (App (Var(add), 1), 2)) |}]
+;;
+
+let%expect_test "parse nested application" =
+  print_string (printer (parse "f (g x)" |> Result.get_ok));
+  [%expect {| App (Var(f), App (Var(g), Var(x))) |}]
+;;
+
+let%expect_test "factorial with rec" =
+  print_string
+    (printer
+       (parse "let rec fac n= if n = 1 then 1 else (fac (n-1)) * n" |> Result.get_ok));
+  [%expect
+    {|(let rec fac = Fun (Var(n), if ((Var(n) = 1)) then (1) else ((App (Var(fac), App (Var(n), -1)) * Var(n)))))|}]
+;;
+
+let%expect_test "some skope values" =
+  let expresion =
+    "let result =\n\
+    \  let sterter x = if x + (8 * 9) > 4 then x * 2 else x * 5 in\n\
+    \    let inner pp = sterter pp + 8 in\n\
+    \  inner 8"
+  in
+  print_string (printer (parse expresion |> Result.get_ok));
+  [%expect
+    {|(let result = (let sterter = Fun (Var(x), if (((Var(x) + (8 * 9)) > 4)) then ((Var(x) * 2)) else ((Var(x) * 5))) in (let inner = Fun (Var(pp), (App (Var(sterter), Var(pp)) + 8)) in App (Var(inner), 8))))|}]
+;;
+
+let%expect_test "function annotation via fun" =
+  print_string (printer (parse "let function = fun a -> a * a" |> Result.get_ok));
+  [%expect {|(let function = Fun (Var(a), (Var(a) * Var(a))))|}]
+;;
+
+let%expect_test "application with lambda" =
+  print_string (printer (parse "let tmp = (fun a -> a * a) 5" |> Result.get_ok));
+  [%expect {|(let tmp = App (Fun (Var(a), (Var(a) * Var(a))), 5))|}]
+;;
+
+let%expect_test "unbounded lambda" =
+  print_string (printer (parse "(fun a -> a * a)" |> Result.get_ok));
+  [%expect {|Fun (Var(a), (Var(a) * Var(a)))|}]
+;;
+
+let%expect_test "simple application" =
+  prerr_string (printer (parse "let _ = g 4 (f 5)" |> Result.get_ok));
+  [%expect {|(let _ = App (App (Var(g), 4), App (Var(f), 5)))|}]
+;;
+
+let%expect_test "nested let with application" =
+  print_string
+    (printer (parse "let f = fun x -> x + 1 in let y = f 5 in y * 2" |> Result.get_ok));
+  [%expect
+    {| (let f = Fun (Var(x), (Var(x) + 1)) in (let y = App (Var(f), 5) in (Var(y) * 2))) |}]
+;;
+
+let%expect_test "anonymous function in application" =
+  print_string (printer (parse "(fun x -> x * 2) 10" |> Result.get_ok));
+  [%expect {| App (Fun (Var(x), (Var(x) * 2)), 10) |}]
+;;
+
+let%expect_test "multiple applications with arithmetic" =
+  print_string (printer (parse "f (g x) + h (k y) * 2" |> Result.get_ok));
+  [%expect
+    {| (App (Var(f), App (Var(g), Var(x))) + (App (Var(h), App (Var(k), Var(y))) * 2)) |}]
+;;
+
+let%expect_test "conditional with applications" =
+  print_string (printer (parse "if f x > 0 then g y else h z" |> Result.get_ok));
+  [%expect
+    {| if ((App (Var(f), Var(x)) > 0)) then (App (Var(g), Var(y))) else (App (Var(h), Var(z))) |}]
+;;
+
+let%expect_test "nested anonymous functions" =
+  print_string (printer (parse "fun x -> fun y -> x + y" |> Result.get_ok));
+  [%expect {| Fun (Var(x), Fun (Var(y), (Var(x) + Var(y)))) |}]
+;;
+
+let%expect_test "let with multiple arguments and application" =
+  print_string (printer (parse "let add x y = x + y in add (f 1) (g 2)" |> Result.get_ok));
+  [%expect
+    {| (let add = Fun (Var(x), Fun (Var(y), (Var(x) + Var(y)))) in App (App (Var(add), App (Var(f), 1)), App (Var(g), 2))) |}]
+;;
+
+let%expect_test "application chain with comparison" =
+  print_string (printer (parse "f a b > g c d + 1" |> Result.get_ok));
+  [%expect
+    {| (App (App (Var(f), Var(a)), Var(b)) > (App (App (Var(g), Var(c)), Var(d)) + 1)) |}]
+;;
+
+let%expect_test "nested lets with applications" =
+  print_string
+    (printer
+       (parse "let x = 5 in let f = fun y -> x + y in let z = f 10 in z * 2"
+        |> Result.get_ok));
+  [%expect
+    {| (let x = 5 in (let f = Fun (Var(y), (Var(x) + Var(y))) in (let z = App (Var(f), 10) in (Var(z) * 2)))) |}]
+;;
+
+let%expect_test "application with complex expressions" =
+  print_string
+    (printer (parse "f (x + 1) (y * 2) (if z > 0 then a else b)" |> Result.get_ok));
+  [%expect
+    {| App (App (App (Var(f), (Var(x) + 1)), (Var(y) * 2)), if ((Var(z) > 0)) then (Var(a)) else (Var(b))) |}]
+;;
+
+let%expect_test "recursive function with application" =
+  print_string
+    (printer
+       (parse "let rec fact n = if n = 0 then 1 else n * fact (n - 1) in fact 5"
+        |> Result.get_ok));
+  [%expect
+    {| (let rec fact = Fun (Var(n), if ((Var(n) = 0)) then (1) else ((Var(n) * App (Var(fact), (Var(n) - 1))))) in App (Var(fact), 5)) |}]
+;;
