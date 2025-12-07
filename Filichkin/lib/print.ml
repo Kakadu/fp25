e986cc8dcf5baf689cd396f8f488c5067c36ddc6
Bogban893 <bogbanrvd@gmail.com>
2025-12-07 23:34:57 +0300

    add print_ast and print_expr

---
+++ Filichkin/lib/print.ml
@@ -0,0 +1,96 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2021-2024, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
+open Ast
+
+let rec print_ast = function
+  | Int n -> Printf.sprintf "Int %d" n
+  | Var x -> Printf.sprintf "Var \"%s\"" x
+  | BinOp (op, l, r) ->
+    let op_str =
+      match op with
+      | Plus -> "Plus"
+      | Minus -> "Minus"
+      | Mult -> "Mult"
+      | Div -> "Div"
+      | Equal -> "Equal"
+      | More -> "More"
+      | Less -> "Less"
+      | ELess -> "ELess"
+      | EMore -> "EMore"
+    in
+    Printf.sprintf "BinOp (%s, %s, %s)" op_str (print_ast l) (print_ast r)
+  | If (c, t, None) -> Printf.sprintf "If (%s, %s, None)" (print_ast c) (print_ast t)
+  | If (c, t, Some e) ->
+    Printf.sprintf "If (%s, %s, Some %s)" (print_ast c) (print_ast t) (print_ast e)
+  | Let (NonRec, x, e, None) ->
+    Printf.sprintf "Let (NonRec, \"%s\", %s, None)" x (print_ast e)
+  | Let (NonRec, x, e, Some b) ->
+    Printf.sprintf "Let (NonRec, \"%s\", %s, Some %s)" x (print_ast e) (print_ast b)
+  | Let (Rec, x, e, None) -> Printf.sprintf "Let (Rec, \"%s\", %s, None)" x (print_ast e)
+  | Let (Rec, x, e, Some b) ->
+    Printf.sprintf "Let (Rec, \"%s\", %s, Some %s)" x (print_ast e) (print_ast b)
+  | Abs (params, body) ->
+    let params_str =
+      List.map (fun p -> Printf.sprintf "\"%s\"" p) params
+      |> String.concat "; "
+      |> Printf.sprintf "[%s]"
+    in
+    Printf.sprintf "Abs (%s, %s)" params_str (print_ast body)
+  | App (f, a) -> Printf.sprintf "App (%s, %s)" (print_ast f) (print_ast a)
+  | Seq lst ->
+    let lst_str = List.map print_ast lst |> String.concat "; " |> Printf.sprintf "[%s]" in
+    Printf.sprintf "Seq %s" lst_str
+  | Fix e -> Printf.sprintf "Fix %s" (print_ast e)
+;;
+
+let rec print_expr = function
+  | Int n -> string_of_int n
+  | Var s -> s
+  | BinOp (op, left, right) ->
+    let oper =
+      match op with
+      | Plus -> "+"
+      | Minus -> "-"
+      | Mult -> "*"
+      | Div -> "/"
+      | Equal -> "="
+      | More -> ">"
+      | Less -> "<"
+      | EMore -> ">="
+      | ELess -> "<="
+    in
+    Printf.sprintf "%s %s %s" (print_expr left) oper (print_expr right)
+  | If (cond, thn, els) ->
+    let else_branch =
+      match els with
+      | None -> ""
+      | Some else_branch -> Printf.sprintf " else %s" (print_expr else_branch)
+    in
+    Printf.sprintf "if %s then %s%s" (print_expr cond) (print_expr thn) else_branch
+  | Let (rec_f, name, value, body) ->
+    let rec_branch =
+      match rec_f with
+      | Rec -> "rec "
+      | NonRec -> ""
+    in
+    let body_branch =
+      match body with
+      | None -> ""
+      | Some body_branch -> Printf.sprintf "in %s" (print_expr body_branch)
+    in
+    Printf.sprintf "let %s%s = %s%s" rec_branch name (print_expr value) body_branch
+  | Abs (params, body) ->
+    let params_str = String.concat " " params in
+    Printf.sprintf "(fun %s -> %s)" params_str (print_expr body)
+  | App (func, arg) -> Printf.sprintf "(%s %s)" (print_expr func) (print_expr arg)
+  | Seq exprs ->
+    let exprs_str = List.map print_expr exprs |> String.concat "; " in
+    Printf.sprintf "(%s)" exprs_str
+  | Fix expr -> Printf.sprintf "(fix %s)" (print_expr expr)
+;;
