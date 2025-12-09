0b58bc4286e290714e13d24898a36e699c0b3f4a
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-12-09 17:47:42 +0300

    feat: add miniML expression parser, support let/rec bindings and conditionals

--- Yackovlev/lib/parser.ml
+++ Yackovlev/lib/parser.ml
@@ -2,9 +2,9 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-(* TODO: implement parser here *)
 open Angstrom
 
+(* Whitespace parser reused everywhere *)
 let is_space = function
   | ' ' | '\t' | '\n' | '\r' -> true
   | _ -> false
@@ -12,15 +12,53 @@ let is_space = function
 
 let spaces = skip_while is_space
 
-let varname =
-  satisfy (function
-    | 'a' .. 'z' -> true
-    | _ -> false)
+(* Consume spaces around parser [p] *)
+let token p = spaces *> p <* spaces
+
+(* Parse a fixed symbol, skipping spaces around it *)
+let symbol s = token (string s)
+
+(* Parenthesised expression *)
+let parens p = symbol "(" *> p <* symbol ")"
+
+(* Identifiers and integers *)
+
+let is_ident_start = function
+  | 'a' .. 'z' | 'A' .. 'Z' | '_' -> true
+  | _ -> false
 ;;
 
-let conde = function
-  | [] -> fail "empty conde"
-  | h :: tl -> List.fold_left ( <|> ) h tl
+let is_ident_char = function
+  | 'a' .. 'z'
+  | 'A' .. 'Z'
+  | '0' .. '9'
+  | '_'
+  | '\'' -> true
+  | _ -> false
+;;
+
+let ident_raw =
+  satisfy is_ident_start
+  >>= fun first ->
+  take_while is_ident_char
+  >>| fun rest -> String.make 1 first ^ rest
+;;
+
+(* [ident] parses variable and function names, including [print_int] *)
+let ident = token ident_raw
+
+let integer =
+  let is_digit = function
+    | '0' .. '9' -> true
+    | _ -> false
+  in
+  let number =
+    option 1 (char '-' *> return (-1))
+    >>= fun sign ->
+    take_while1 is_digit
+    >>| fun ds -> sign * int_of_string ds
+  in
+  token number
 ;;
 
 type dispatch =
@@ -34,32 +72,136 @@ let pp_error ppf = function
   | `Parsing_error s -> Format.fprintf ppf "%s" s
 ;;
 
+(* Main expression parser for miniML *)
+let expr : string Ast.t Angstrom.t =
+  let open Ast in
+  fix (fun expr ->
+    (* [chainl1 p op] parses left associative binary operators like [a + b + c] *)
+    let chainl1 p op =
+      p
+      >>= fun init ->
+      many (op >>= fun f -> p >>| fun v -> (f, v))
+      >>| fun rest ->
+      List.fold_left (fun acc (f, v) -> f acc v) init rest
+    in
+
+    (* Atomic expressions: integers, variables and parenthesised expressions *)
+    let atom =
+      choice
+        [ parens expr
+        ; integer >>| fun n -> Int n
+        ; ident >>| fun x -> Var x
+        ]
+    in
+
+    (* Function application is left associative and tighter than any infix operator *)
+    let app =
+      many1 atom
+      >>| function
+      | [ x ] -> x
+      | x :: xs -> List.fold_left (fun acc r -> App (acc, r)) x xs
+      | [] -> failwith "application on empty list, impossible by [many1]"
+    in
+
+    (* Multiplication and division, left associative *)
+    let mul_div =
+      let op =
+        choice
+          [ symbol "*" *> return (fun l r -> Binop (Mul, l, r))
+          ; symbol "/" *> return (fun l r -> Binop (Div, l, r))
+          ]
+      in
+      chainl1 app op
+    in
+
+    (* Addition and subtraction, left associative *)
+    let add_sub =
+      let op =
+        choice
+          [ symbol "+" *> return (fun l r -> Binop (Add, l, r))
+          ; symbol "-" *> return (fun l r -> Binop (Sub, l, r))
+          ]
+      in
+      chainl1 mul_div op
+    in
+
+    (* Comparison operators have the lowest precedence among infix operators *)
+    let cmp =
+      let op =
+        choice
+          [ symbol "=" *> return (fun l r -> Cmp (Eq, l, r))
+          ; symbol "<>" *> return (fun l r -> Cmp (Neq, l, r))
+          ; symbol "<=" *> return (fun l r -> Cmp (Le, l, r))
+          ; symbol ">=" *> return (fun l r -> Cmp (Ge, l, r))
+          ; symbol "<" *> return (fun l r -> Cmp (Lt, l, r))
+          ; symbol ">" *> return (fun l r -> Cmp (Gt, l, r))
+          ]
+      in
+      chainl1 add_sub op
+    in
+
+    (* Desugar multi argument functions to nested [Abs] *)
+    let make_fun params body =
+      List.fold_right (fun x acc -> Abs (x, acc)) params body
+    in
+
+    (* [fun x y -> e] sugar for nested abstractions *)
+    let fun_expr =
+      symbol "fun"
+      *> many1 ident
+      >>= fun params ->
+      symbol "->"
+      *> expr
+      >>| fun body -> make_fun params body
+    in
+
+    (* [if e1 then e2 else e3] expression, lowest precedence *)
+    let if_expr =
+      symbol "if"
+      *> expr
+      >>= fun cond ->
+      symbol "then"
+      *> expr
+      >>= fun then_ ->
+      symbol "else"
+      *> expr
+      >>| fun else_ -> If (cond, then_, else_)
+    in
+
+    (* [let] and [let rec] with optional curried arguments *)
+    let let_expr =
+      symbol "let"
+      *> peek_string 3
+      >>= fun s ->
+      let is_rec = s = "rec" in
+      (if is_rec then string "rec" *> spaces else return ())
+      *> ident
+      >>= fun name ->
+      many ident
+      >>= fun params ->
+      symbol "="
+      *> expr
+      >>= fun rhs ->
+      symbol "in"
+      *> expr
+      >>| fun body ->
+      let rhs' = make_fun params rhs in
+      if is_rec then Let_rec (name, rhs', body) else Let (name, rhs', body)
+    in
+
+    (* Statement like forms have the lowest precedence, so we try them first *)
+    choice [ let_expr; if_expr; fun_expr; cmp ])
+;;
+
+(* Keep [dispatch] around for compatibility, but both entries now parse full expressions *)
 let parse_lam =
-  let single pack =
-    fix (fun _ ->
-      conde
-        [ char '(' *> pack.apps pack <* char ')' <?> "Parentheses expected"
-        ; ((string "λ" <|> string "\\") *> spaces *> varname
-           <* spaces
-           <* (return () <* char '.' <|> string "->" *> return ())
-           >>= fun var ->
-           pack.apps pack >>= fun b -> return (Ast.Abs (String.make 1 var, b)))
-        ; (varname <* spaces >>= fun c -> return (Ast.Var (String.make 1 c)))
-        ])
-  in
-  let apps pack =
-    many1 (spaces *> pack.single pack <* spaces)
-    >>= function
-    | [] -> fail "bad syntax"
-    | x :: xs -> return @@ List.fold_left (fun l r -> Ast.App (l, r)) x xs
-  in
-  { single; apps }
+  let apps _ = expr in
+  let single _ = expr in
+  { apps; single }
 ;;
 
 let parse str =
-  match
-    Angstrom.parse_string (parse_lam.apps parse_lam) ~consume:Angstrom.Consume.All str
-  with
+  match Angstrom.parse_string (expr) ~consume:Angstrom.Consume.All str with
   | Result.Ok x -> Result.Ok x
   | Error er -> Result.Error (`Parsing_error er)
 ;;
