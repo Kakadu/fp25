ef27b7bdad61d5be7c1a865ccd68110b933f8a75
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-12-09 18:05:48 +0300

    feat(print): pretty print miniML expressions

--- Yackovlev/lib/Pprintast.ml
+++ Yackovlev/lib/Pprintast.ml
@@ -6,59 +6,119 @@
 
 [@@@ocaml.text "/*"]
 
-(* Pretty printer goes here *)
-
 open Ast
-open Utils
 
-let pp ?(compact = true) =
-  let open Format in
-  let mangle t fmt x =
-    if is_free_in x t || not compact then fprintf fmt "%s" x else fprintf fmt "_"
-  in
-  let rec pp fmt = function
-    | Var s -> Format.fprintf fmt "%s" s
-    | App (l, r) -> Format.fprintf fmt "(%a %a)" pp l pp r
-    | Abs (x, Abs (y, Var z)) when x = z && y <> z && compact ->
-      if compact then Format.fprintf fmt "⊤"
-    | Abs (x, Abs (y, Var z)) when y = z && x <> z && compact -> Format.fprintf fmt "⊥"
-    | Abs (f, Abs (x, Var z)) when x = z && x <> f && compact -> Format.fprintf fmt "0"
-    | Abs (f, Abs (x, App (Var g, Var z))) when x = z && x <> f && g = f && compact ->
-      Format.fprintf fmt "1"
-    | Abs (f, Abs (x, App (Var g, App (Var h, Var z))))
-      when x = z && x <> f && g = f && h = g && compact -> Format.fprintf fmt "2"
-    | Abs (v1, Abs (v2, Abs (v3, Abs (v4, t)))) when compact ->
-      Format.fprintf
-        fmt
-        "(λ %a %a %a %a -> %a)"
-        (mangle t)
-        v1
-        (mangle t)
-        v2
-        (mangle t)
-        v3
-        (mangle t)
-        v4
-        pp
-        t
-    | Abs (v1, Abs (v2, Abs (v3, t))) when compact ->
-      Format.fprintf
-        fmt
-        "(λ %a %a %a -> %a)"
-        (mangle t)
-        v1
-        (mangle t)
-        v2
-        (mangle t)
-        v3
-        pp
-        t
-    | Abs (v1, Abs (v2, t)) when compact ->
-      Format.fprintf fmt "(λ %a %a -> %a)" (mangle t) v1 (mangle t) v2 pp t
-    | Abs (x, t) -> Format.fprintf fmt "(λ %a . %a)" (mangle t) x pp t
-  in
-  pp
+let pp_binop fmt = function
+  | Add -> Format.fprintf fmt "+"
+  | Mul -> Format.fprintf fmt "*"
+  | Sub -> Format.fprintf fmt "-"
+  | Div -> Format.fprintf fmt "/"
 ;;
 
-let pp_hum = pp ~compact:true
-let pp = pp ~compact:false
+let pp_cmpop fmt = function
+  | Eq -> Format.fprintf fmt "="
+  | Neq -> Format.fprintf fmt "<>"
+  | Lt -> Format.fprintf fmt "<"
+  | Le -> Format.fprintf fmt "<="
+  | Gt -> Format.fprintf fmt ">"
+  | Ge -> Format.fprintf fmt ">="
+;;
+
+(* Flatten nested [Abs] into parameter list for [fun x y -> e] sugar *)
+let rec collect_fun_params acc = function
+  | Abs (x, body) -> collect_fun_params (x :: acc) body
+  | body -> List.rev acc, body
+;;
+
+(* Precedence levels
+   0: let, let rec, if, fun
+   1: comparison operators
+   2: + and -
+   3: * and /
+   4: application
+   5: atomic expressions
+*)
+let rec pp_expr prec fmt = function
+  | Var x -> Format.fprintf fmt "%s" x
+  | Int n -> Format.fprintf fmt "%d" n
+  | App (e1, e2) ->
+    let my_prec = 4 in
+    if prec > my_prec then Format.fprintf fmt "(";
+    pp_expr my_prec fmt e1;
+    Format.fprintf fmt " ";
+    pp_expr (my_prec + 1) fmt e2;
+    if prec > my_prec then Format.fprintf fmt ")"
+  | Abs (x, body) as e ->
+    let params, body = collect_fun_params [ x ] body in
+    let my_prec = 0 in
+    if prec > my_prec then Format.fprintf fmt "(";
+    Format.fprintf fmt "fun";
+    List.iter (fun p -> Format.fprintf fmt " %s" p) params;
+    Format.fprintf fmt " -> ";
+    pp_expr my_prec fmt body;
+    if prec > my_prec then Format.fprintf fmt ")"
+  | Binop (op, e1, e2) ->
+    let my_prec =
+      match op with
+      | Mul | Div -> 3
+      | Add | Sub -> 2
+    in
+    if prec > my_prec then Format.fprintf fmt "(";
+    pp_expr my_prec fmt e1;
+    Format.fprintf fmt " %a " pp_binop op;
+    pp_expr (my_prec + 1) fmt e2;
+    if prec > my_prec then Format.fprintf fmt ")"
+  | Cmp (op, e1, e2) ->
+    let my_prec = 1 in
+    if prec > my_prec then Format.fprintf fmt "(";
+    pp_expr my_prec fmt e1;
+    Format.fprintf fmt " %a " pp_cmpop op;
+    pp_expr (my_prec + 1) fmt e2;
+    if prec > my_prec then Format.fprintf fmt ")"
+  | Let (x, e1, e2) ->
+    let my_prec = 0 in
+    if prec > my_prec then Format.fprintf fmt "(";
+    (* Desugar back [let f x y = body] when RHS is a lambda *)
+    (match e1 with
+     | Abs (p, body) ->
+       let params, fun_body = collect_fun_params [ p ] body in
+       Format.fprintf fmt "let %s" x;
+       List.iter (fun p -> Format.fprintf fmt " %s" p) params;
+       Format.fprintf fmt " = ";
+       pp_expr my_prec fmt fun_body
+     | _ ->
+       Format.fprintf fmt "let %s = " x;
+       pp_expr my_prec fmt e1);
+    Format.fprintf fmt " in ";
+    pp_expr my_prec fmt e2;
+    if prec > my_prec then Format.fprintf fmt ")"
+  | Let_rec (f, e1, e2) ->
+    let my_prec = 0 in
+    if prec > my_prec then Format.fprintf fmt "(";
+    (match e1 with
+     | Abs (p, body) ->
+       let params, fun_body = collect_fun_params [ p ] body in
+       Format.fprintf fmt "let rec %s" f;
+       List.iter (fun p -> Format.fprintf fmt " %s" p) params;
+       Format.fprintf fmt " = ";
+       pp_expr my_prec fmt fun_body
+     | _ ->
+       Format.fprintf fmt "let rec %s = " f;
+       pp_expr my_prec fmt e1);
+    Format.fprintf fmt " in ";
+    pp_expr my_prec fmt e2;
+    if prec > my_prec then Format.fprintf fmt ")"
+  | If (cond, then_, else_) ->
+    let my_prec = 0 in
+    if prec > my_prec then Format.fprintf fmt "(";
+    Format.fprintf fmt "if ";
+    pp_expr 1 fmt cond;
+    Format.fprintf fmt " then ";
+    pp_expr my_prec fmt then_;
+    Format.fprintf fmt " else ";
+    pp_expr my_prec fmt else_;
+    if prec > my_prec then Format.fprintf fmt ")"
+;;
+
+let pp fmt e = pp_expr 0 fmt e
+let pp_hum = pp
--- Yackovlev/lib/Printast.ml
+++ Yackovlev/lib/Printast.ml
@@ -6,10 +6,32 @@
 
 [@@@ocaml.text "/*"]
 
+type binop = Ast.binop =
+  | Add
+  | Mul
+  | Sub
+  | Div
+[@@deriving show { with_path = false }]
+
+type cmpop = Ast.cmpop =
+  | Eq
+  | Neq
+  | Lt
+  | Le
+  | Gt
+  | Ge
+[@@deriving show { with_path = false }]
+
 type 'name t = 'name Ast.t =
   | Var of 'name
+  | Int of int
   | Abs of 'name * 'name t
   | App of 'name t * 'name t
+  | Let of 'name * 'name t * 'name t
+  | Let_rec of 'name * 'name t * 'name t
+  | If of 'name t * 'name t * 'name t
+  | Binop of binop * 'name t * 'name t
+  | Cmp of cmpop * 'name t * 'name t
 [@@deriving show { with_path = false }]
 
 let pp_named = pp Format.pp_print_string
