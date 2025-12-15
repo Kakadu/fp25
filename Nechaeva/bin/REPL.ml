7e46d6e0babb9a599822b18bb4f22410a854a6f9
Darya Nechaeva <necaevadara435@gmail.com>
2025-12-15 22:35:06 +0300

    refactor REPL file

--- Nechaeva/bin/REPL.ml
+++ Nechaeva/bin/REPL.ml
@@ -18,6 +18,7 @@ let string_of_value = function
   | VClosure _ -> "<closure>"
   | VRecClosure _ -> "<rec_closure>"
   | VBuiltin _ -> "<builtin>"
+;;
 
 let string_of_error = function
   | UnboundVariable name -> Printf.sprintf "Unbound variable: %s" name
@@ -25,32 +26,28 @@ let string_of_error = function
   | DivisionByZero -> "Division by zero"
   | NotAFunction -> "Not a function"
   | ExceedMaxSteps -> "Maximum steps exceeded"
+;;
 
 let run_single opts =
   let text = In_channel.(input_all stdin) |> String.trim in
   match parse text with
-  | Error (`Parsing_error msg) -> 
-    Format.printf "Parser error: %s\n" msg
+  | Error (`Parsing_error msg) -> Format.printf "Parser error: %s\n" msg
   | Ok ast ->
-    if opts.show_ast then 
-      Format.printf "AST: %a\n" pp_expr ast;
-    
-    match run_interpreter ast opts.max_steps with
-    | Ok value ->
-      Format.printf "Result: %s\n" (string_of_value value)
-    | Error err ->
-      Format.printf "Error: %s\n" (string_of_error err)
+    if opts.show_ast then Format.printf "AST: %a\n" pp_expr ast;
+    (match run_interpreter ast opts.max_steps with
+     | Ok value -> Format.printf "Result: %s\n" (string_of_value value)
+     | Error err -> Format.printf "Error: %s\n" (string_of_error err))
+;;
 
 let () =
   let opts = { show_ast = false; max_steps = 1000 } in
-  
   let arg_list =
-    [ ("--ast", Arg.Unit (fun () -> opts.show_ast <- true), "Show AST before evaluation")
-    ; ("--maxSteps", Arg.Int (fun n -> opts.max_steps <- n), 
-       "Set maximum number of evaluation steps (default: 1000)")
+    [ "--ast", Arg.Unit (fun () -> opts.show_ast <- true), "Show AST before evaluation"
+    ; ( "--maxSteps"
+      , Arg.Int (fun n -> opts.max_steps <- n)
+      , "Set maximum number of evaluation steps (default: 1000)" )
     ]
   in
-  
   Arg.parse arg_list (fun _ -> ()) "";
-  
   run_single opts
+;;
