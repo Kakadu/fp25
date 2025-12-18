696a92e1efcb1e2d10ce0be0ddc3ddf5d88a8720
Darya Nechaeva <necaevadara435@gmail.com>
2025-12-18 13:02:18 +0300

    fix formatting

--- Nechaeva/lib/ast.mli
+++ Nechaeva/lib/ast.mli
@@ -5,52 +5,42 @@
 type name = string
 
 (** Recursion flag for let bindings *)
-type rec_flag = 
-  | NonRecursive    (** [let x = e1 in e2] *)
-  | Recursive       (** [let rec f = e1 in e2] *)
+type rec_flag =
+  | NonRecursive (** [let x = e1 in e2] *)
+  | Recursive (** [let rec f = e1 in e2] *)
 
 (** Binary operations *)
 type binop =
-  | Plus     (** Integer addition: + *)
-  | Minus    (** Integer subtraction: - *)
-  | Mul      (** Integer multiplication: * *)
-  | Div      (** Integer division: / *)
+  | Plus (** Integer addition: + *)
+  | Minus (** Integer subtraction: - *)
+  | Mul (** Integer multiplication: * *)
+  | Div (** Integer division: / *)
 
 (** Comparison operations *)
 type compop =
-  | Equal       (** Equal: = *)
-  | NotEqual    (** Not equal: <> *)
-  | Less        (** Less than: < *)
-  | LessEq      (** Less or equal: <= *)
-  | Greater     (** Greater than: > *)
-  | GreaterEq   (** Greater or equal: >= *)
+  | Equal (** Equal: = *)
+  | NotEqual (** Not equal: <> *)
+  | Less (** Less than: < *)
+  | LessEq (** Less or equal: <= *)
+  | Greater (** Greater than: > *)
+  | GreaterEq (** Greater or equal: >= *)
 
 (** Constant values *)
 type constant =
-  | Int of int    (** Integer literal *)
-  | Unit          (** Unit value *)
+  | Int of int (** Integer literal *)
+  | Unit (** Unit value *)
 
 (** Unary operations *)
-type unop =
-  | Neg    (** Unary minus: -e *)
+type unop = Neg (** Unary minus: -e *)
 
 (** Expressions *)
 type expr =
-  | Const of constant
-    (** Constant value *)
-  | Var of name
-    (** Variable reference *)
-  | Abs of name list * expr
-    (** Multi-argument abstraction: [fun x y -> e] *)
-  | App of expr * expr
-    (** Function application: [f arg] *)
-  | Let of rec_flag * name * expr * expr
-    (** Let binding: [let[rec] x = e1 in e2] *)
-  | BinOp of binop * expr * expr
-    (** Binary arithmetic operation *)
-  | UnOp of unop * expr
-    (** Unary operation *)
-  | Comp of compop * expr * expr
-    (** Comparison operation *)
-  | If of expr * expr * expr
-    (** Conditional: [if cond then e1 else e2] *)
+  | Const of constant (** Constant value *)
+  | Var of name (** Variable reference *)
+  | Abs of name list * expr (** Multi-argument abstraction: [fun x y -> e] *)
+  | App of expr * expr (** Function application: [f arg] *)
+  | Let of rec_flag * name * expr * expr (** Let binding: [let[rec] x = e1 in e2] *)
+  | BinOp of binop * expr * expr (** Binary arithmetic operation *)
+  | UnOp of unop * expr (** Unary operation *)
+  | Comp of compop * expr * expr (** Comparison operation *)
+  | If of expr * expr * expr (** Conditional: [if cond then e1 else e2] *)
--- Nechaeva/lib/interpret.mli
+++ Nechaeva/lib/interpret.mli
@@ -1,4 +1,5 @@
 (** Copyright 2021-2025, Kakadu and contributors *)
+
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 open Ast
@@ -7,7 +8,7 @@ type error =
   | UnboundVariable of string
   | TypeError of string
   | DivisionByZero
-  | NotAFunction 
+  | NotAFunction
   | ExceedMaxSteps
 
 type value =
@@ -21,21 +22,18 @@ type env = (string * value) list
 
 module StepErrorM : sig
   type 'a t = int -> int * ('a, error) result
-  
+
   val return : 'a -> 'a t
   val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
   val fail : error -> 'a t
-  
   val get_steps : int t
   val dec_steps : unit t
-  
   val run : 'a t -> int -> int * ('a, error) result
 end
 
 val env_empty : env
 val env_extend : env -> string -> value -> env
 val env_lookup : env -> string -> (value, error) result
-
 val initial_env : env
 
 module Interpreter : sig
