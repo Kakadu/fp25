1a2ed34558a51842ae029edf0f422b2d7cef9248
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-12-21 20:44:09 +0300

    feat: rethink repl logic

--- Yackovlev/bin/REPL.ml
+++ Yackovlev/bin/REPL.ml
@@ -8,31 +8,24 @@
 
 open Lambda_lib
 
-module I = Interpret
+let main () =
+  (* Read all input from stdin until EOF (Ctrl+D) *)
+  let input = In_channel.input_all In_channel.stdin in
 
-let prompt = "# "
+  match Parser.parse input with
+  | Error (`Parsing_error err) ->
+    Format.printf "Error: %s\n%!" err
+  | Ok ast ->
+    (* Print parsed AST using the pretty printer *)
+    Format.printf "%a\n%!" Printast.pp_named ast;
 
-let rec repl_loop () =
-  (* Print prompt and flush stdout immediately using %! *)
-  Format.printf "%s%!" prompt;
-  match Stdlib.read_line () with
-  | exception End_of_file ->
-    (* Handle Ctrl+D (EOF) gracefully to exit the loop *)
-    ()
-  | line ->
-    let line = String.trim line in
-    if String.equal line "quit"
-    then
-      (* Exit on specific command *)
-      ()
-    else (
-      (* Process input if it's not empty *)
-      (if String.length line > 0
-       then
-         match I.run_program line with
-         | Ok (v, _fuel) -> Format.printf "- : %s\n%!" (I.string_of_value v)
-         | Error err -> Format.printf "Error: %s\n%!" (I.string_of_run_error err));
-      repl_loop ())
+    (* Evaluate *)
+    let fuel = 100_000 in
+    match Interpret.eval Interpret.initial_env fuel ast with
+    | Ok (v, _fuel_left) ->
+      Format.printf "%s\n%!" (Interpret.string_of_value v)
+    | Error err ->
+      Format.printf "Error: %s\n%!" (Interpret.string_of_error err)
 ;;
 
-let () = repl_loop ()
+let () = main ()
