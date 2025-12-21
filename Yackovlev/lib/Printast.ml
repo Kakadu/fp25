51bd42c0eeeb917a2a7823faa880ee436f308505
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-12-21 20:47:50 +0300

    feat: change printing logic

--- Yackovlev/lib/Printast.ml
+++ Yackovlev/lib/Printast.ml
@@ -6,32 +6,55 @@
 
 [@@@ocaml.text "/*"]
 
-type binop = Ast.binop =
-  | Add
-  | Sub
-  | Mul
-  | Div
-[@@deriving show { with_path = false }]
+open Format
+open Ast
 
-type cmpop = Ast.cmpop =
-  | Eq
-  | Neq
-  | Lt
-  | Le
-  | Gt
-  | Ge
-[@@deriving show { with_path = false }]
+let string_of_binop = function
+  | Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | Div -> "/"
+;;
 
-type 'name t = 'name Ast.t =
-  | Var of 'name
-  | Int of int
-  | Abs of 'name * 'name t
-  | App of 'name t * 'name t
-  | Let of 'name * 'name t * 'name t
-  | Let_rec of 'name * 'name t * 'name t
-  | If of 'name t * 'name t * 'name t
-  | Binop of binop * 'name t * 'name t
-  | Cmp of cmpop * 'name t * 'name t
-[@@deriving show { with_path = false }]
+let string_of_cmpop = function
+  | Eq -> "="
+  | Neq -> "<>"
+  | Lt -> "<"
+  | Le -> "<="
+  | Gt -> ">"
+  | Ge -> ">="
+;;
 
-let pp_named = pp Format.pp_print_string
+let rec pp pp_name fmt = function
+  | Var x -> pp_name fmt x
+  | Int n -> fprintf fmt "%d" n
+  | Abs (x, e) -> fprintf fmt "fun %a -> %a" pp_name x (pp pp_name) e
+  | App (e1, e2) -> fprintf fmt "(%a %a)" (pp pp_name) e1 (pp pp_name) e2
+  | Let (x, e1, e2) ->
+    fprintf fmt "let %a = %a in %a" pp_name x (pp pp_name) e1 (pp pp_name) e2
+  | Let_rec (f, e1, e2) ->
+    fprintf fmt "let rec %a = %a in %a" pp_name f (pp pp_name) e1 (pp pp_name) e2
+  | If (cond, t, e) ->  
+    fprintf fmt "if %a then %a else %a" (pp pp_name) cond (pp pp_name) t (pp pp_name) e
+  | Binop (op, e1, e2) ->
+    fprintf
+      fmt
+      "%a %s %a"
+      (pp pp_name)
+      e1
+      (string_of_binop op)
+      (pp pp_name)
+      e2
+  | Cmp (op, e1, e2) ->
+    fprintf
+      fmt
+      "%a %s %a"
+      (pp pp_name)
+      e1
+      (string_of_cmpop op)
+      (pp pp_name)
+      e2
+;;
+
+let pp_named fmt ast = pp pp_print_string fmt ast
+let show pp_name ast = asprintf "%a" (pp pp_name) ast
