3e8db58d159ff6f3b38c87b7b0ba9d576e54588e
Yackovlev Nickolay <yackovlevnick0@gmail.com>
2025-12-22 02:14:57 +0300

    feat: add tests

--- Yackovlev/tests/interpret_tests.t
+++ Yackovlev/tests/interpret_tests.t
@@ -1,4 +1,4 @@
-Copyright 2021-2024, Kakadu and contributors
+Copyright 2021-2025, Kakadu and contributors
 SPDX-License-Identifier: CC0-1.0
 
 Cram tests here. They run and compare program output to the expected output
@@ -8,3 +8,64 @@ Use `dune promote` after you change things that should runned
 If you need to put sample program and use it both in your interpreter and preinstalled one,
 you could put it into separate file. Thise will need stanza `(cram (deps demo_input.txt))`
 in the dune file
+
+  $ REPL=./../bin/REPL.exe
+
+  $ $REPL <<EOF
+  > 1 + 2 * 3
+  (1 + (2 * 3))
+  7
+
+  $ $REPL <<EOF
+  > 10 / 0
+  (10 / 0)
+  Error: Division by zero
+
+  $ $REPL <<EOF
+  > if 1 < 2 then 100 else 200
+  (if (1 < 2) then 100 else 200)
+  100
+
+  $ $REPL <<EOF
+  > if 5 then 1 else 0
+  (if 5 then 1 else 0)
+  1
+
+  $ $REPL <<EOF
+  > let x = 5 in let y = x + 1 in x * y
+  (let x = 5 in (let y = (x + 1) in (x * y)))
+  30
+
+  $ $REPL <<EOF
+  > let f = fun x y z -> x + y + z in f 1 2 3
+  (let f = (fun x -> (fun y -> (fun z -> ((x + y) + z)))) in (((f 1) 2) 3))
+  6
+
+  $ $REPL <<EOF
+  > let rec fact n = if n <= 1 then 1 else n * fact (n - 1) in fact 5
+  (let rec fact = (fun n -> (if (n <= 1) then 1 else (n * (fact (n - 1))))) in (fact 5))
+  120
+
+  $ $REPL <<EOF
+  > let rec fib n = if n <= 1 then n else fib (n-1) + fib (n-2) in fib 10
+  (let rec fib = (fun n -> (if (n <= 1) then n else ((fib (n - 1)) + (fib (n - 2))))) in (fib 10))
+  55
+
+  $ $REPL <<EOF
+  > let rec fix f x = f (fix f) x in
+  > let fact_gen fact n = if n = 0 then 1 else n * fact (n - 1) in
+  > let fact = fix fact_gen in
+  > fact 5
+  (let rec fix = (fun f -> (fun x -> ((f (fix f)) x))) in (let fact_gen = (fun fact -> (fun n -> (if (n = 0) then 1 else (n * (fact (n - 1)))))) in (let fact = (fix fact_gen) in (fact 5))))
+  120
+
+  $ $REPL <<EOF
+  > let rec loop x = loop x in loop 0
+  (let rec loop = (fun x -> (loop x)) in (loop 0))
+  Error: Out of fuel
+
+  $ $REPL <<EOF
+  > let _ = print_int 999 in 0
+  (let _ = (print_int 999) in 0)
+  999
+  0
---
+++ Yackovlev/tests/parser_test.ml
@@ -0,0 +1,79 @@
+(** Copyright 2021-2025, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Lambda_lib
+open Ast
+
+let gen_name =
+  let open QCheck.Gen in
+  oneofl ["x"; "y"; "z"; "a"; "b"; "c"; "f"; "g"; "n"; "m"; "res"; "fact"; "fib"]
+
+let gen_binop =
+  let open QCheck.Gen in
+  oneofl [Add; Sub; Mul; Div]
+
+let gen_cmpop =
+  let open QCheck.Gen in
+  oneofl [Eq; Neq; Lt; Le; Gt; Ge]
+
+let gen_expr =
+  let open QCheck.Gen in
+  let rec expr depth =
+    if depth <= 0 then
+      oneof [ map (fun i -> Int i) (int_range (-100) 100)
+            ; map (fun s -> Var s) gen_name ]
+    else
+      let new_depth = depth - 1 in
+      frequency
+        [ (3, map (fun i -> Int i) (int_range (-100) 100))
+        ; (3, map (fun s -> Var s) gen_name)
+        ; (1, map3 (fun op l r -> Binop (op, l, r)) gen_binop (expr new_depth) (expr new_depth))
+        ; (1, map3 (fun op l r -> Cmp (op, l, r)) gen_cmpop (expr new_depth) (expr new_depth))
+        ; (1, map2 (fun name body -> Abs (name, body)) gen_name (expr new_depth))
+        ; (2, map2 (fun e1 e2 -> App (e1, e2)) (expr new_depth) (expr new_depth))
+        ; (1, map3 (fun c t e -> If (c, t, e)) (expr new_depth) (expr new_depth) (expr new_depth))
+        ; (1, map3 (fun x e1 e2 -> Let (x, e1, e2)) gen_name (expr new_depth) (expr new_depth))
+        ; (1, map3 (fun x e1 e2 -> Let_rec (x, e1, e2)) gen_name (expr new_depth) (expr new_depth))
+        ]
+  in
+  sized (fun n -> expr (min n 5))
+
+let print_expr e = Printast.show (fun fmt s -> Format.fprintf fmt "%s" s) e
+
+let arbitrary_expr = QCheck.make ~print:print_expr gen_expr
+
+let test_print_parse_roundtrip =
+  QCheck.Test.make ~count:100 ~name:"Roundtrip: Parse(Print(e)) == e" arbitrary_expr
+    (fun ast ->
+      let str = print_expr ast in
+      match Parser.parse str with
+      | Ok ast' -> ast = ast'
+      | Error _ -> false)
+
+let test_printer_safety =
+  QCheck.Test.make ~count:100 ~name:"Printer Safety" arbitrary_expr
+    (fun ast ->
+      try let _ = print_expr ast in true
+      with _ -> false)
+
+let test_parser_negative =
+  let invalid_inputs = 
+    [ ""; "let x ="; "1 +"; "if true then"; "fun -> x"; "let rec 1 = x" ] 
+  in
+  let gen_invalid = QCheck.Gen.oneofl invalid_inputs in
+  QCheck.Test.make ~count:(List.length invalid_inputs) ~name:"Negative Parser Tests" 
+    (QCheck.make ~print:(fun s -> s) gen_invalid)
+    (fun str ->
+      match Parser.parse str with
+      | Error (`Parsing_error _) -> true
+      | Ok _ -> false )
+
+let () =
+  let tests = 
+    [ test_print_parse_roundtrip
+    ; test_printer_safety
+    ; test_parser_negative 
+    ] 
+  in
+  QCheck_runner.run_tests_main tests
