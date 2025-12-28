172b4b70141b5eb960affbac894ed142cecafe3f
Tenyaeva Ekaterina <b08lsoai@gmail.com>
2025-12-29 01:27:48 +0300

    ref: improve test_round_trip
    Signed-off-by: Tenyaeva Ekaterina <b08lsoai@gmail.com>

--- Tenyaeva/tests/qcheck.ml
+++ Tenyaeva/tests/qcheck.ml
@@ -2,24 +2,31 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-(* open Tenyaeva_lib.Ast *)
 open Tenyaeva_lib.Parser
 open Tenyaeva_lib.Pprinter
 open Generators
+open Stdlib.Format
 
 let arbitrary_structure =
   QCheck.make gen_structure ~print:(Format.asprintf "%a" pp_structure)
 ;;
 
-let run n =
-  QCheck_base_runner.run_tests
-    [ QCheck.Test.make ~count:n arbitrary_structure (fun s ->
-        (* Stdlib.Format.printf "%s\n" (show_structure s); *)
-        Result.ok s = parse (Format.asprintf "%a" pp_structure s))
-    ]
+let test_round_trip =
+  QCheck.Test.make
+    ~name:"AST -> pretty-printer -> parser -> AST"
+    ~count:10_000
+    arbitrary_structure
+    (fun structure ->
+       let printed_structure = asprintf "%a" pp_structure structure in
+       match parse printed_structure with
+       | Ok parsed -> parsed = structure
+       | Error err ->
+         printf "Generated program:\n%s\n\n" printed_structure;
+         printf "Parsing failed with error: %s\n" err;
+         false)
 ;;
 
 let () =
-  let _ = run 3 in
+  let _ : int = QCheck_base_runner.run_tests [ test_round_trip ] in
   ()
 ;;
