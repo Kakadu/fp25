a6509f7510b0492c8fc67316800f5a66ceca7065
talubik <rustem149849@gmail.com>
2026-01-02 20:47:48 +0300

    add : classes and other in ast

--- OOP_Shaehmitov_Motuzov/lib/ast/ast.ml
+++ OOP_Shaehmitov_Motuzov/lib/ast/ast.ml
@@ -35,6 +35,7 @@ type pattern =
   | PVar of name
   | PAny
   | PUnit
+  | PTuple of pattern list
 [@@deriving show { with_path = false }]
 
 type rec_flag =
@@ -43,7 +44,19 @@ type rec_flag =
 [@@deriving show { with_path = false }]
 
 (** Unary operators *)
-type unop = Neg (** '-' *) [@@deriving show { with_path = false }]
+type unop =
+  | Neg (** '-' *)
+  | Not (** 'not' **)
+[@@deriving show { with_path = false }]
+
+type typ =
+  | TInt (** Integer type *)
+  | TBool (** Boolean type *)
+  | TUnit (** Unit type *)
+  | TFun of typ * typ (** Function type: T1 -> T2 *)
+  | TTuple of typ list (** Tuple type: T1 * T2 * ... * Tn *)
+  | TClass of name (** Class with a name *)
+[@@deriving show { with_path = false }]
 
 (** Expressions in the AST *)
 type expr =
@@ -55,16 +68,48 @@ type expr =
   | Let of rec_flag * pattern * expr * expr (** Local binding, e.g. `let x = e1 in e2` *)
   | FunExpr of pattern list * expr (** Abstraction (function), e.g. `fun x -> e` *)
   | App of expr * expr (** Function application, e.g. `f x` *)
+  | Tuple of expr list (** Tuple, e.g. `(e1, e2, ..., en)` *)
+  | New of class_def (** Class instantiation, e.g. `new ClassName` *)
+  | MethodCall of expr * name * expr list (** Method call, e.g. `obj#methodName(args)` *)
+  | FieldAccess of expr * name (** Field access, e.g. `obj#fieldName` *)
+[@@deriving show { with_path = false }]
+
+and method_def =
+  { method_name : name
+  ; method_params : pattern list
+  ; method_body : expr
+  }
+[@@deriving show { with_path = false }]
+
+and field_def = name * expr [@@deriving show { with_path = false }]
+
+and class_def =
+  { class_name : name
+  ; parent_class : name option
+  ; fields : field_def list
+  ; methods : method_def list
+  }
 [@@deriving show { with_path = false }]
 
 type binding = rec_flag * pattern * expr [@@deriving show { with_path = false }]
-type structure_item = Value of binding [@@deriving show { with_path = false }]
+
+type structure_item =
+  | Value of binding
+  | ClassDef of class_def
+[@@deriving show { with_path = false }]
+
 type program = structure_item list [@@deriving show { with_path = false }]
 
-let pp_pattern fmt = function
+let rec pp_pattern fmt = function
   | PVar s -> Format.fprintf fmt "%s" s
   | PAny -> Format.fprintf fmt "_"
   | PUnit -> Format.fprintf fmt "()"
+  | PTuple ps ->
+    Format.fprintf
+      fmt
+      "(%a)"
+      (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ", ") pp_pattern)
+      ps
 ;;
 
 let rec pp_expr fmt = function
@@ -76,6 +121,7 @@ let rec pp_expr fmt = function
     let op_str =
       match op with
       | Neg -> "-"
+      | Not -> "not "
     in
     Format.fprintf fmt "(%s%a)" op_str pp_expr e
   | BinOp (op, e1, e2) ->
@@ -109,12 +155,53 @@ let rec pp_expr fmt = function
   | Let (rec_flag, p, e1, e2) ->
     let rec_str = if rec_flag = Rec then " rec" else "" in
     Format.fprintf fmt "(let%s %a = %a in %a)" rec_str pp_pattern p pp_expr e1 pp_expr e2
+  | Tuple es ->
+    Format.fprintf
+      fmt
+      "(%a)"
+      (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ", ") pp_expr)
+      es
+  | New class_def -> Format.fprintf fmt "(new %a)" pp_class_def class_def
+  | MethodCall (obj, method_name, args) ->
+    Format.fprintf
+      fmt
+      "(%a#%s(%a))"
+      pp_expr
+      obj
+      method_name
+      (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt ", ") pp_expr)
+      args
+  | FieldAccess (obj, field_name) -> Format.fprintf fmt "(%a#%s)" pp_expr obj field_name
+
+and pp_method_def fmt { method_name; method_params; method_body } =
+  Format.fprintf
+    fmt
+    "method %s %a = %a"
+    method_name
+    (Format.pp_print_list ~pp_sep:Format.pp_print_space pp_pattern)
+    method_params
+    pp_expr
+    method_body
+
+and pp_field_def fmt (name, init_expr) =
+  Format.fprintf fmt "val %s = %a" name pp_expr init_expr
+
+and pp_class_def fmt { class_name; parent_class; fields; methods } =
+  Format.fprintf fmt "class %s" class_name;
+  (match parent_class with
+   | Some parent -> Format.fprintf fmt " extends %s" parent
+   | None -> ());
+  Format.fprintf fmt " {\n";
+  List.iter (fun f -> Format.fprintf fmt "  %a;\n" pp_field_def f) fields;
+  List.iter (fun m -> Format.fprintf fmt "  %a;\n" pp_method_def m) methods;
+  Format.fprintf fmt "}"
 ;;
 
 let pp_program_item fmt = function
   | Value (rec_flag, p, e) ->
     let rec_str = if rec_flag = Rec then " rec" else "" in
     Format.fprintf fmt "let%s %a = %a" rec_str pp_pattern p pp_expr e
+  | ClassDef class_def -> pp_class_def fmt class_def
 ;;
 
 let pp_program fmt p =
