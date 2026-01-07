8abb38c8aa840ede9e94fda1ff3bbf99011e0e79
RamProsad2 <ramprosad.chandrasutradhar@gmail.com>
2026-01-07 20:14:53 +0300

    fix type error

--- EChandraSutraDhar/lib/interpreter.ml
+++ EChandraSutraDhar/lib/interpreter.ml
@@ -6,9 +6,15 @@ open Ast
 open Base
 open Stdlib.Format
 
-type env = (ident, value, String.comparator_witness) Map.t
 
-and value =
+type value_error =
+  | UnboundVariable of ident
+  | TypeError
+  | DivisionByZeroError
+  | PatternMatchingError
+  | LHS
+
+type value =
   | ValueInt of int
   | ValueBool of bool
   | ValueString of string
@@ -19,12 +25,7 @@ and value =
   | ValueOption of value option
   | ValueBuiltin of (value -> (value, value_error) Result.t)
 
-and value_error =
-  | UnboundVariable of ident
-  | TypeError
-  | DivisionByZeroError
-  | PatternMatchingError
-  | LHS
+and env = (ident, value, String.comparator_witness) Map.t
 
 let pp_value_error fmt = function
   | UnboundVariable ident -> fprintf fmt "UnboundVariable: %S" ident
