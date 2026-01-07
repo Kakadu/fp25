4ee41dee0494da6681e5b046863727a61ca35144
RamProsad2 <ramprosad.chandrasutradhar@gmail.com>
2026-01-07 20:26:26 +0300

    fix type error

--- EChandraSutraDhar/lib/interpreter.ml
+++ EChandraSutraDhar/lib/interpreter.ml
@@ -6,15 +6,9 @@ open Ast
 open Base
 open Stdlib.Format
 
+type env = (ident, value, String.comparator_witness) Map.t
 
-type value_error =
-  | UnboundVariable of ident
-  | TypeError
-  | DivisionByZeroError
-  | PatternMatchingError
-  | LHS
-
-type value =
+and value =
   | ValueInt of int
   | ValueBool of bool
   | ValueString of string
@@ -25,7 +19,12 @@ type value =
   | ValueOption of value option
   | ValueBuiltin of (value -> (value, value_error) Result.t)
 
-and env = (ident, value, String.comparator_witness) Map.t
+and value_error =
+  | UnboundVariable of ident
+  | TypeError
+  | DivisionByZeroError
+  | PatternMatchingError
+  | LHS
 
 let pp_value_error fmt = function
   | UnboundVariable ident -> fprintf fmt "UnboundVariable: %S" ident
--- EChandraSutraDhar/lib/interpreter.mli
+++ EChandraSutraDhar/lib/interpreter.mli
@@ -5,15 +5,9 @@
 open Ast
 open Base
 
+type env = (ident, value, String.comparator_witness) Map.t
 
-type value_error =
-  | UnboundVariable of ident
-  | TypeError
-  | DivisionByZeroError
-  | PatternMatchingError
-  | LHS
-
-type value =
+and value =
   | ValueInt of int
   | ValueBool of bool
   | ValueString of string
@@ -24,7 +18,12 @@ type value =
   | ValueOption of value option
   | ValueBuiltin of (value -> (value, value_error) Result.t)
 
-and env = (ident, value, String.comparator_witness) Map.t
+and value_error =
+  | UnboundVariable of ident
+  | TypeError
+  | DivisionByZeroError
+  | PatternMatchingError
+  | LHS
 
 val pp_value_error : Stdlib.Format.formatter -> value_error -> unit
 
