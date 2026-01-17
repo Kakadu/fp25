0bd463771bab8fe5f9079f5132fc4eebdf613786
Tenyaeva Ekaterina <b08lsoai@gmail.com>
2026-01-18 00:44:39 +0300

    ref: remove unnecessary code
    Signed-off-by: Tenyaeva Ekaterina <b08lsoai@gmail.com>

--- Tenyaeva/lib/monad.ml
+++ Tenyaeva/lib/monad.ml
@@ -2,35 +2,11 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-module type STATER = sig
-  type 'a t
-
-  val return : 'a -> 'b -> 'b * ('a, 'c) result
-  val fail : 'a -> 'b -> 'b * ('c, 'a) result
-
-  val ( >>= )
-    :  ('a -> 'b * ('c, 'd) result)
-    -> ('c -> 'b -> 'b * ('e, 'd) result)
-    -> 'a
-    -> 'b * ('e, 'd) result
-
-  val ( let* )
-    :  ('a -> 'b * ('c, 'd) result)
-    -> ('c -> 'b -> 'b * ('e, 'd) result)
-    -> 'a
-    -> 'b * ('e, 'd) result
-
-  val ( >>| ) : ('a -> 'b * ('c, 'd) result) -> ('c -> 'e) -> 'a -> 'b * ('e, 'd) result
-  val ( let+ ) : ('a -> 'b * ('c, 'd) result) -> ('c -> 'e) -> 'a -> 'b * ('e, 'd) result
-  val get : 'a -> 'a * ('a, 'b) result
-  val put : 'a -> 'b -> 'a * (unit, 'c) result
-  val run : ('a -> 'b) -> 'a -> 'b
-end
-
 module StateR (S : sig
     type state
     type error
-  end) : STATER = struct
+  end) =
+struct
   type state = S.state
   type error = S.error
   type 'a t = state -> state * ('a, error) result
