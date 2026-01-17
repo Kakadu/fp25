ec8c6f212bb71af902efe90c52450ea89699c8ae
psiblvdegod <artyombaly@list.ru>
2026-01-18 01:33:17 +0300

    update inferencer signature
    Signed-off-by: psiblvdegod <artyombaly@list.ru>

--- Balyshev/lib/inferencer.mli
+++ Balyshev/lib/inferencer.mli
@@ -1,27 +1,24 @@
-type error =
-  | Not_implemented of string
-  | Occurs_check of Typedtree.ty * Typedtree.ty
-  | Unification_failed of Typedtree.ty * Typedtree.ty
-  | Type_mismatch
-  | Unbound_value of string
-  | Type_was_not_declared of string
-  | Type_arity_mismatch of string (* TODO : change message *)
-  | Unbound_type_variable of string
-  | Type_param_duplicates of string
-  | Unbound_constructor of string
-  | Type_env_invariant_violation of string
-  | Constructor_arity_mismatch of string
-  | Only_variables_on_the_left_of_let_rec
-  | Only_functions_on_the_right_of_let_rec
+module Error : sig
+  type t =
+    | Not_implemented of string
+    | Occurs_check of string * Typedtree.ty
+    | Unification_failed of Typedtree.ty * Typedtree.ty
+    | Type_was_not_declared of string
+    | Unbound_value of string
+    | Unbound_type_variable of string
+    | Unbound_constructor of string
+    | Type_param_duplicates of string
+    | Constructor_arity_mismatch of string
+    | Left_of_let_rec
+    | Right_of_let_rec
+    | Constructor_name_duplicates of string
+    | TypeEnv_invariant_violation of string
 
-val show_error : error -> string
-val pp_error : Format.formatter -> error -> unit
+  val show : t -> string
+  val pp : Format.formatter -> t -> unit
+end
 
-module Infer (_ : Monads.STATE_MONAD) : sig
-  val expression : ?debug:bool -> Parsetree.expression -> (Typedtree.ty, error) result
-
-  val structure
-    :  ?debug:bool
-    -> Parsetree.structure_item * Parsetree.structure_item list
-    -> (Typedtree.structure_item * Typedtree.structure_item list, error) result
+module Make (_ : Monads.STATE_MONAD) : sig
+  val infer_expression : Parsetree.expression -> (Typedtree.ty, Error.t) result
+  val infer_structure : Parsetree.structure -> (Typedtree.structure, Error.t) result
 end
