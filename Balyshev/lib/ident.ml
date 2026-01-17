675cc228c02142d1d1f46574b4ee9d030a947b68
psiblvdegod <artyombaly@list.ru>
2026-01-18 01:28:00 +0300

    get rid of old unused stuff in ident.ml
    Signed-off-by: psiblvdegod <artyombaly@list.ru>

--- Balyshev/lib/ident.ml
+++ Balyshev/lib/ident.ml
@@ -3,12 +3,11 @@ type t =
   ; name : string
   }
 
-type ident = t
-
 let ident id name = { id; name }
-let equal (a : ident) (b : ident) = a.id = b.id
-let compare (a : ident) (b : ident) = Int.compare a.id b.id
-let show_ident ident = Format.sprintf "'ty%d" ident.id
+let equal (a : t) (b : t) = a.id = b.id
+let compare (a : t) (b : t) = Int.compare a.id b.id
+let show ident = Format.sprintf "'ty%d" ident.id
+let of_int n = ident n (Format.sprintf "'ty%d" n)
 
 module Id_map = Stdlib.Map.Make (Int)
 module Name_map = Stdlib.Map.Make (String)
@@ -26,40 +25,8 @@ module Ident_map = struct
     Id_map.remove id left, Name_map.remove name right
   ;;
 
-  let find_by_id_opt id ((left, _) : _ t) = Id_map.find_opt id left
-  let find_by_name_opt name ((_, right) : _ t) = Name_map.find_opt name right
+  let find_by_id id ((left, _) : _ t) = Id_map.find_opt id left
+  let find_by_name name ((_, right) : _ t) = Name_map.find_opt name right
   let fmap ~f (left, right) = Id_map.map f left, Name_map.map f right
-  let fold_by_ids ~f ~init (left, _) = Id_map.fold f left init
-
-  let fold2 ~f_id ~f_name (left, right) ~init_id ~init_name =
-    Id_map.fold f_id left init_id, Name_map.fold f_name right init_name
-  ;;
-
-  let pp ppf ~pp_data (left, _) =
-    Format.fprintf ppf "<< substitution: {@.";
-    Id_map.iter
-      (fun id data -> Format.fprintf ppf "  'ty%d -> %a@. " id pp_data data)
-      left;
-    Format.fprintf ppf "@.} >>@."
-  ;;
-end
-
-module Ident_map_m (M : Monads.STATE_MONAD) = struct
-  open M
-
-  let fmapm ~f (left, right) =
-    let helper_id key data acc =
-      let* acc = acc in
-      let* data' = f data in
-      return (Id_map.add key data' acc)
-    in
-    let* mapped_ids = Id_map.fold helper_id left (return Id_map.empty) in
-    let helper_names key data acc =
-      let* acc = acc in
-      let* data' = f data in
-      return (Name_map.add key data' acc)
-    in
-    let* mapped_names = Name_map.fold helper_names right (return Name_map.empty) in
-    return (mapped_ids, mapped_names)
-  ;;
+  let fold ~f ~init (left, _) = Id_map.fold f left init
 end
