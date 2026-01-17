7c07e961e7c8b1438c256e6da0c7d6e78dd54eaf
psiblvdegod <artyombaly@list.ru>
2026-01-18 01:31:50 +0300

    add pprint.ml for some aux pretty-printing stuff
    Signed-off-by: psiblvdegod <artyombaly@list.ru>

---
+++ Balyshev/lib/pprint.ml
@@ -0,0 +1,49 @@
+open Base.Format
+open Base
+
+module Parens = struct
+  type context =
+    | Free (** top-level [ let ] and expressions after [ in ] are free *)
+    | Binop (** [ XXX + XXX * (XXX - XXX) ] *)
+    | Tuple (** [ XXX, XXX ]*)
+    | App (** [ XXX XXX ] *)
+    | LeftSideFun (** [ fun XXX -> ... ]  *)
+    | RightSideFun (** [ fun ... -> XXX ]  *)
+    | MatchWith (** [ match XXX with ... ] *)
+    | LeftSideMatch (** [ match ... with | XXX -> ... ] *)
+    | RightSideMatch (** [ match ... with | ... -> XXX ] *)
+    | LeftSideArrow (** [ 'XXX -> ... ] in type declaration and infered types *)
+    | RightSideArrow (** [ ... -> 'XXX ] in type declaration and infered types *)
+    | LeftSideLet (** [ let XXX = ... in ... ] *)
+    | RightSideLet (** [ let ... = XXX in ... ] *)
+    | Ite (** [ if XXX then XXX else XXX ] *)
+
+  let parens s = sprintf "(%s)" s
+
+  let set_parens ~ctx cases s =
+    if List.exists cases ~f:(fun x -> x == ctx) then sprintf "(%s)" s else s
+  ;;
+end
+
+let show_many ~sep show_item items =
+  let pp_sep ppf () = pp_print_string ppf sep in
+  let pp_item ppf item = pp_print_string ppf (show_item item) in
+  asprintf "%a" (pp_print_list ~pp_sep pp_item) items
+;;
+
+let show_tuple show_item (a, b, xs) = show_many ~sep:", " show_item (a :: b :: xs)
+
+let show_list_brackets show_item = function
+  | [] -> "[]"
+  | items -> sprintf "[ %s ]" (show_many ~sep:"; " show_item items)
+;;
+
+let show_list_cons
+      ?(ctx = Parens.Free)
+      (show_item : ?ctx:Parens.context -> 'a -> string)
+      items
+  =
+  let open Parens in
+  set_parens ~ctx [ App; Binop ]
+  @@ String.concat ~sep:" :: " (List.map ~f:(show_item ~ctx:Binop) items)
+;;
