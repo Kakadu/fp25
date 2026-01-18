350353a538b620d9f1bacc816727b83918a3a7b9
psiblvdegod <artyombaly@list.ru>
2026-01-19 01:33:23 +0300

    delete some unused functions in ident.ml
    Signed-off-by: psiblvdegod <artyombaly@list.ru>

--- Balyshev/lib/ident.ml
+++ Balyshev/lib/ident.ml
@@ -32,9 +32,4 @@ module Ident_map = struct
   let remove { id; name } (left, right) =
     Id_map.remove id left, Name_map.remove name right
   ;;
-
-  let find_by_id id ((left, _) : _ t) = Id_map.find_opt id left
-  let find_by_name name ((_, right) : _ t) = Name_map.find_opt name right
-  let fmap ~f (left, right) = Id_map.map f left, Name_map.map f right
-  let fold ~f ~init (left, _) = Id_map.fold f left init
 end
