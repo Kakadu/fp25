c63f015102600d7fc0ebff2e2902f62f0f4464ec
alex <alex@linux.myguest.virtualbox.org>
2026-02-15 23:06:11 +0300

    Исправил часть проблем с интерпретацией

--- ZarubinAlexey/bin/REPL.ml
+++ ZarubinAlexey/bin/REPL.ml
@@ -5,3 +5,13 @@
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 [@@@ocaml.text "/*"]
+
+open Base
+
+let read_all_stdin () : string = In_channel.input_all In_channel.stdin
+
+let () =
+  let input = read_all_stdin () in
+  (* Сейчас мы используем твой существующий entrypoint *)
+  Lambda_lib.Interpret.parse_and_run input
+;;
