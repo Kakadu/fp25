5e0087b1fc63e432a9c21d3397f5e0b0e904ae62
Sterkachov <anymeshnik2.1@gmail.com>
2026-03-03 23:57:23 +0300

    polish qcheck tests

--- Davletshin/tests/gen_tests.ml
+++ Davletshin/tests/gen_tests.ml
@@ -50,73 +50,17 @@ let rec gen_ast depth =
          ])
 ;;
 
-let rec lol = function
-  | Int n -> string_of_int n
-  | Var s -> s
-  | App (l, r) -> "(" ^ lol l ^ " " ^ lol r ^ ")"
-  | Abs (x, t) -> "(fun " ^ x ^ " -> " ^ lol t ^ ")"
-  | Binop (op, l, r) ->
-    let bop =
-      match op with
-      | Plus -> "+"
-      | Minus -> "-"
-      | Times -> "*"
-      | Divide -> "/"
-      | Eq -> "="
-      | Neq -> "<>"
-      | Lt -> "<"
-      | Gt -> ">"
-      | Le -> "<="
-      | Ge -> ">="
-    in
-    "(" ^ lol l ^ " " ^ bop ^ " " ^ lol r ^ ")"
-  | Unop (op, e) ->
-    let uop =
-      match op with
-      | Pos -> "+"
-      | Neg -> "-"
-    in
-    "(" ^ uop ^ lol e ^ ")"
-  | If (c, t, e) -> "(if " ^ lol c ^ " then " ^ lol t ^ " else " ^ lol e ^ ")"
-  | Let (Nonrec, n, e1, e2) -> "(let " ^ n ^ " = " ^ lol e1 ^ " in " ^ lol e2 ^ ")"
-  | Let (Rec, n, e1, e2) -> "(let rec " ^ n ^ " = " ^ lol e1 ^ " in " ^ lol e2 ^ ")"
-  | Fix e -> "(fix " ^ lol e ^ ")"
-  | Print e -> "(print " ^ lol e ^ ")"
-;;
-
-let rec equal e1 e2 =
-  match e1, e2 with
-  | Int x1, Int x2 -> x1 = x2
-  | Var x1, Var x2 -> String.equal x1 x2
-  | Abs (x1, t1), Abs (x2, t2) -> String.equal x1 x2 && equal t1 t2
-  | App (f1, g1), App (f2, g2) -> equal f1 f2 && equal g1 g2
-  | Binop (op1, a1, b1), Binop (op2, a2, b2) -> op1 = op2 && equal a1 a2 && equal b1 b2
-  | Unop (op1, a1), Unop (op2, a2) -> op1 = op2 && equal a1 a2
-  | If (c1, t1, e1), If (c2, t2, e2) -> equal c1 c2 && equal t1 t2 && equal e1 e2
-  | Let (f1, p1, b1, e1), Let (f2, p2, b2, e2) ->
-    f1 = f2 && String.equal p1 p2 && equal b1 b2 && equal e1 e2
-  | Fix e1, Fix e2 -> equal e1 e2
-  | Print e1, Print e2 -> equal e1 e2
-  | _ -> false
-;;
-
+let equal = Ast.equal String.equal
 let pprint ast = Format.asprintf "%a" Pprintast.pp ast
 let gen_ast_sized = Gen.sized (fun s -> gen_ast (3 + (s mod 3)))
-let arb_ast = make ~print:lol gen_ast_sized
-
-let parse_test =
-  Test.make ~count:200 ~name:"parser test" arb_ast (fun ast ->
-    match Parser.parse (lol ast) with
-    | Result.Ok ast' -> equal ast ast'
-    | Result.Error _ -> false)
-;;
+let arb_ast = make ~print:pprint gen_ast_sized
 
 let pprint_test =
-  Test.make ~count:200 ~name:"pprint test" arb_ast (fun ast ->
+  Test.make ~count:1000 ~name:"pprint& parse test" arb_ast (fun ast ->
     match Parser.parse (pprint ast) with
     | Result.Ok ast' -> equal ast ast'
     | Result.Error _ -> false)
 ;;
 
 (*TODO: gen_varname can generate a keyword*)
-let _ = QCheck_runner.run_tests [ parse_test; pprint_test ]
+let _ = QCheck_runner.run_tests [ pprint_test ]
