41646e045faaf55ead87e5212983817167699891
Andrey <andrey_kalashnikov_am@list.ru>
2026-03-05 02:32:50 +0000

    add ast and parser

---
+++ KalashnikovAndrey/lib/ast.mli
@@ -0,0 +1,31 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2021-2024, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
+type op = 
+| Add
+| Sub
+| Mul
+| Div
+| Lt
+| Eq
+| Mt
+
+type rec_flag =
+| Rec
+| Val
+
+type  expr = 
+| Const of int
+| BinOp of op * expr * expr
+| Var of string
+| Let of  rec_flag * string * expr * expr
+| If of expr * expr * expr
+| App of expr * expr 
+| Fun of string * expr 
+| Neg of expr
+| Fix of expr
---
+++ KalashnikovAndrey/lib/parser.mli
@@ -0,0 +1,15 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2021-2024, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+open Ast
+type error = [ `Parsing_error of string ]
+
+val pp_error : Format.formatter -> [< `Parsing_error of string ] -> unit
+
+(** Main entry of parser *)
+val parse : string -> (Ast.expr, [> error ]) result
+
