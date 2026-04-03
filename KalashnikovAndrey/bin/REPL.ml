8d2b60ddac56021df597bcaf2018f6fffbdc9cbb
Andrey <andrey_kalashnikov_am@list.ru>
2026-04-03 18:54:31 +0000

    repl implementation

--- KalashnikovAndrey/bin/REPL.ml
+++ KalashnikovAndrey/bin/REPL.ml
@@ -1,10 +1,13 @@
 [@@@ocaml.text "/*"]
+
 (** Copyright 2021-2024, Kakadu and contributors *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 [@@@ocaml.text "/*"]
+
 open Lambda_lib
+
 let prompt = "> "
 
 let read_max_steps () =
--- KalashnikovAndrey/bin/REPL.mli
+++ KalashnikovAndrey/bin/REPL.mli
@@ -5,3 +5,12 @@
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 [@@@ocaml.text "/*"]
+
+val prompt : string
+
+val read_max_steps : unit -> int
+
+val read_printer_flag : unit -> bool
+
+val repl : int -> bool -> unit
+
